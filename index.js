54587642107de3ba6b7ecababd2c38a5a79f160f
fsexchangerrates <fs.suppermaster@gmail.com>
2020-06-28 08:50:17 +0700

    update

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const server = require('./server');
+const env = require('./.env');
+const { config } = require('dotenv/types');
+const { path } = require('./server');
+
+server.use(config(data.join(__dirname, "dotenv"), ".env"))
 
 server.listen(3000 || process.env.PORT, () => {
     console.log(`Server is running on localhost:3000`);
